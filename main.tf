6b404e9e3b874275231103d6fe5dcaa9a86dad4a
Antony Potter <tone@potteringabout.net>
2024-02-10 09:23:51 +0000

    Initial config

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ module "website" {
   source             = "./modules/website"
 
   providers = {
-    aws = aws
-    aws.us-east-1 = aws.us_east_1
+    aws = aws.us-east-1
   }
 }
--- provider.tf
+++ provider.tf
@@ -34,7 +34,7 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias = "us_east_1"
+  alias = "us-east-1"
 
-  region = "es-east-1"
+  region = "us-east-1"
 }
